a0961e9a56bf42a0c47039d11d068a14b46bd119
Cole Cianflone <colecianflone123@gmail.com>
2022-11-07 15:14:38 -0600

    Add files via upload
    creates a category

---
+++ create_category.php
@@ -0,0 +1,38 @@
+<?php
+/*
+    Cole Cianflone
+    Nov 7th, 2022
+    Purpose: Script to create a category.
+*/
+
+require("connect.php");
+require("admin.php");
+
+
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>category - Edit </title>
+      <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+   </head>
+   <body>
+
+         <div id="category_to_create">
+            <form action="process_category_post.php" method="post">
+               <fieldset>
+                  <div class="blog_post">
+                     <p>
+                        <label for="category_name">Category Name</label>
+                        <input name="category_name" id="category_name" value=""/>
+                     </p>
+                     <p>
+                        <input type="submit" name="command" value="Create" />
+                     </p>
+                  </div>
+               </fieldset>
+            </form>
+         </div>
+   </body>
+</html>
