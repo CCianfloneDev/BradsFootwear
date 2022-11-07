5a39fb2f826cc44123787a3bfa41b474da346201
Cole Cianflone <colecianflone123@gmail.com>
2022-11-07 14:09:40 -0600

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,27 @@
+<?php
+/*
+    Cole Cianflone
+    Sept 26th, 2022
+    Purpose: Script to authenticate users to site.
+*/
+
+$query = "SELECT * FROM user WHERE admin_access = 1";
+$statement = $db->prepare($query); // Returns a PDOStatement object.
+$statement->execute(); // The query is now executed.
+$users = $statement->fetchAll();
+
+define("ADMIN_LOGIN", $users[0]['user_name']);
+define("ADMIN_PASSWORD", $users[0]['user_pass']);
+
+if (
+    !isset($_SERVER["PHP_AUTH_USER"]) ||
+    !isset($_SERVER["PHP_AUTH_PW"]) ||
+    $_SERVER["PHP_AUTH_USER"] != ADMIN_LOGIN ||
+    $_SERVER["PHP_AUTH_PW"] != ADMIN_PASSWORD
+) {
+    header("HTTP/1.1 401 Unauthorized");
+    header('WWW-Authenticate: Basic realm="Our Blog"');
+    exit("Access Denied: Username and password required.");
+}
+
+?>
