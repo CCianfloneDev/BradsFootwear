cac9e15f2a3854d205287f9fd1bbe358a2508ead
Cole Cianflone <colecianflone123@gmail.com>
2022-11-07 15:26:55 -0600

    Add files via upload
    implementation to edit brands

---
+++ edit_brand.php
@@ -0,0 +1,51 @@
+<?php
+/*
+    Cole Cianflone
+    Nov 7th, 2022
+    Purpose: Script to edit a brand.
+*/
+
+require("connect.php");
+require("admin.php");
+
+// gets id of post
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+// prepare query
+$query = "SELECT * FROM sneaker_manufacturer WHERE sneaker_brand_id = :id";
+$statement = $db->prepare($query);
+
+// bind values to insert statement
+$statement->bindValue(":id", $id, PDO::PARAM_INT);
+$statement->execute();
+$brands = $statement->fetchAll();
+
+?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>brand - Edit </title>
+      <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+   </head>
+   <body>
+
+         <div id="brand_to_edit">
+            <form action="process_brand_post.php" method="post">
+               <fieldset>
+                  <div class="blog_post">
+                     <p>
+                        <label for="sneaker_brand">Brand Name</label>
+                        <input name="sneaker_brand" id="sneaker_brand" value="<?=$brands[0]['sneaker_brand']?>"/>
+                     </p>
+                     <p>
+                        <input type="hidden" name="sneaker_brand_id" value="<?=$_GET['id']?>" />
+                        <input type="submit" name="command" value="Update" />
+                        <input type="submit" name="command" value="Delete" onclick="return confirm('Are you sure you wish to delete this brand?')" />
+                     </p>
+                  </div>
+               </fieldset>
+            </form>
+         </div>
+   </body>
+</html>
