701290a3cf4e17bbc49aee7cb885a839819aaeae
Cole Cianflone <colecianflone123@gmail.com>
2022-11-07 13:59:22 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+    require "connect.php";
+
+    /* SELECT ALL DATA FROM sneaker_categorys */
+    // $query = "SELECT * FROM sneaker_category";
+    // $statement = $db->prepare($query); // Returns a PDOStatement object.
+    // $statement->execute(); // The query is now executed.
+    // $categories = $statement->fetchAll();
+
+    
+    /* SELECT ALL DATA FROM user */
+    // $query = "SELECT * FROM user";
+    // $statement = $db->prepare($query); // Returns a PDOStatement object.
+    // $statement->execute(); // The query is now executed.
+    // $users = $statement->fetchAll();
+?>
+
+<!DOCTYPE html>
+<html>
+   <head>
+      <meta charset="utf-8">
+      <title>Final WebDev Project</title>
+      <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+   </head>
+   <body>
+
+   </body>
+</html>
