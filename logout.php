6083809ce04c7b7c33bfc2a1db4bda1c7d4e8077
Cole Cianflone <colecianflone123@gmail.com>
2022-11-10 15:35:33 -0600

    Update logout.php
    Added redirect if logout selected from view sneaker page

--- logout.php
+++ logout.php
@@ -9,7 +9,13 @@ require("connect.php");
 session_start();
 session_destroy();
 
-//Redirect after logout.
-header("Location: index.php");
+// Redirect after logout.
+if ($_GET['redirect'] == "view_sneaker")
+{
+    header("Location: view_sneaker.php?id=".$_GET['id']);
+} else 
+{
+    header("Location: index.php");
+}
 exit;
 ?>
