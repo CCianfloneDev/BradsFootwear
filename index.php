9a7ffaf34d015883ca0d008cdd3fe5fea5d6ea0d
Cole Cianflone <colecianflone123@gmail.com>
2022-11-10 17:07:48 -0600

    Update index.php
    added functionality to search sneakers.

--- index.php
+++ index.php
@@ -20,7 +20,19 @@
     $statement->execute(); // The query is now executed.
     $brands = $statement->fetchAll();
 
+    if ($_POST && $_POST['command'] == 'Search')
+    {
+        $search_input = filter_input(INPUT_POST, 'search_input', FILTER_SANITIZE_STRING);
+        $search = "%".$search_input."%";
 
+        /* SELECT DATA FROM sneakers table */
+        $query = "SELECT * FROM sneaker WHERE sneaker_name LIKE :search ORDER BY sneaker_name DESC";
+        $statement = $db->prepare($query);
+        // bind values to insert statement
+        $statement->bindValue(":search", $search);
+        $statement->execute();
+        $searchResult = $statement->fetchAll();
+    }
 ?>
 
 <!DOCTYPE html>
@@ -45,6 +57,32 @@
         <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
         <h2><a href="create_sneaker.php">Create sneaker</a></h2>
         <?php endif ?>
+        <div id="search_form">
+            <form action="index.php" method="post">
+               <fieldset>
+                  <div class="search">
+                  <h2>Search for sneakers!</h2>
+                     <p>
+                        <input name="search_input" type="text" id="search_input" style="width:330px;" placeholder="Search by name of sneaker, value, or size of sneaker..." value=""/>
+                     </p>
+                     <p>
+                        <input type="submit" name="command" value="Search" />
+                     </p>
+                  </div>
+               </fieldset>
+            </form>
+         </div>
+        <?php if($_POST) :?>
+            <h2><a href="index.php?reset=true">View all sneakers</a></h2>
+            <?php foreach($searchResult as $sneaker):?>
+                <h2>Sneaker Name: <?=$sneaker['sneaker_name']?> 
+                || <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
+                <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
+                || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
+                <?php endif ?>
+            </h2>
+            <?php endforeach ?>
+        <?php elseif(!$_POST || $_GET['reset'] == true): ?>
         <?php foreach($sneakers as $sneaker): ?>
             <h2>Sneaker Name: <?=$sneaker['sneaker_name']?> 
                 || <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
@@ -53,6 +91,7 @@
                 <?php endif ?>
             </h2>
         <?php endforeach ?>
+        <?php endif ?>
     </div>
     <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1): ?>
         <div id="all_categories">
