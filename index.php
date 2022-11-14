918651ab1f06a5e031f9eff38673a144d47d1826
Cole Cianflone <colecianflone123@gmail.com>
2022-11-14 15:28:19 -0600

    Update index.php
    optimized how searching/sorting works. added bootstrap

--- index.php
+++ index.php
@@ -45,13 +45,19 @@
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
    <head>
       <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>HOME</title>
-      <!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
    </head>
    <body>
+   <div class="container-fluid">
+    
    <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
             <h5><a href="admin_user_moderation.php">User Account Moderation</a></h5>
         <?php endif ?>
@@ -61,16 +67,18 @@
     <?php if(isset($_SESSION['logged_in_user'])):?>
         <h1><a href="logout.php">Log Out</a></h1>
         <?php else:?>
-            <h1><a href="login.php">Sign in</a></h1>
-            <h1><a href="register.php">Register</a></h1>
+            <p><a href="login.php">Sign in</a></p>
+            <p><a href="register.php">Register</a></p>
     <?php endif ?>
-   <div id="all_footwear">
+    
+   </div>
+   <div class="container" id="all_footwear">
         <h1>Sneakers</h1>
         <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
         <h2><a href="create_sneaker.php">Create sneaker</a></h2>
         <?php endif ?>
         <div id="search_form">
-            <form action="index.php" method="post">
+            <form action="index.php?searched=true" method="post">
                <fieldset>
                   <div class="search">
                   <h2>Search for sneakers!</h2>
@@ -84,14 +92,26 @@
                </fieldset>
             </form>
          </div>
+            <?php if(!isset($_GET['searched'])):?>
+         <div class="row">
+        <div class="col-sm">
          <h1><a href="index.php">Default sort</a></h1>
-         <h1><a href="index.php?sort=name">Sort by name &#9660</a></h1>
-         <h1><a href="index.php?sort=brand">Sort by brand &#9660</a></h1>
-         <h1><a href="index.php?sort=price">Sort by price &#9660</a></h1>
+        </div>
+        <div class="col-sm">
+         <h1><a href="index.php?sort=name">Sort by name &#9660;</a></h1>
+        </div>
+         <div class="col-sm">
+         <h1><a href="index.php?sort=brand">Sort by brand &#9660;</a></h1>
+         </div>
+         <div class="col-sm">
+         <h1><a href="index.php?sort=price">Sort by price &#9660;</a></h1>
+         </div>
+        </div>
+        <?php endif ?>
         <?php if($_POST) :?>
             <h2><a href="index.php?reset=true">View all sneakers</a></h2>
             <?php foreach($searchResult as $sneaker):?>
-                <h2>Sneaker Name: <?=$sneaker['sneaker_name']?> 
+                <h2><?=$sneaker['sneaker_name']?> 
                 || <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
                 <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
                 || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
@@ -100,7 +120,7 @@
             <?php endforeach ?>
         <?php elseif(!$_POST || $_GET['reset'] == true): ?>
         <?php foreach($sneakers as $sneaker): ?>
-            <h2>Sneaker Name: <?=$sneaker['sneaker_name']?> 
+            <h2><?=$sneaker['sneaker_name']?> 
                 || <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
                 <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
                 || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
