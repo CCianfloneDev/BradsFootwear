82a18e7785711b4c457212b1d422c1c0e820cf5f
Cole Cianflone <colecianflone123@gmail.com>
2022-11-15 13:29:41 -0600

    Update index.php
    Implemented a template header, optimized admin functionality, cleaned up a bit with bootstrap.

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <?php
     require("connect.php");
-    session_start();
+    require("templates/header.php");
 
-    /* SELECT ALL DATA FROM sneaker_categorys table*/
-    $query = "SELECT * FROM sneaker_category";
-    $statement = $db->prepare($query); // Returns a PDOStatement object.
-    $statement->execute(); // The query is now executed.
-    $categories = $statement->fetchAll();
-
-    /* SELECT ALL DATA FROM sneakers table */
     if (isset($_GET['sort']) && $_GET['sort'] == 'name'){
         $query = "SELECT * FROM sneaker ORDER BY sneaker_name DESC";
     } else if(isset($_GET['sort']) && $_GET['sort'] == 'brand') {
@@ -19,15 +12,10 @@
         $query = "SELECT * FROM sneaker";
     }
 
+    /* SELECT ALL DATA FROM sneakers table */
     $statement = $db->prepare($query); // Returns a PDOStatement object.
     $statement->execute(); // The query is now executed.
     $sneakers = $statement->fetchAll();
-
-    /* SELECT ALL DATA FROM sneaker_manufacturer table */
-    $query = "SELECT * FROM sneaker_manufacturer";
-    $statement = $db->prepare($query); // Returns a PDOStatement object.
-    $statement->execute(); // The query is now executed.
-    $brands = $statement->fetchAll();
 
     if ($_POST && $_POST['command'] == 'Search')
     {
@@ -43,34 +31,15 @@
         $searchResult = $statement->fetchAll();
     }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>HOME</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
    </head>
    <body>
    <div class="container-fluid">
-    
-   <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
-            <h5><a href="admin_user_moderation.php">User Account Moderation</a></h5>
-        <?php endif ?>
-    <?php if(isset($_SESSION['logged_in_user'])):?>
-        <h1>Thanks for logging in <em><?=$_SESSION['logged_in_user']?></em></h1>
-    <?php endif ?>
-    <?php if(isset($_SESSION['logged_in_user'])):?>
-        <h1><a href="logout.php">Log Out</a></h1>
-        <?php else:?>
-            <p><a href="login.php">Sign in</a></p>
-            <p><a href="register.php">Register</a></p>
-    <?php endif ?>
-    
    </div>
    <div class="container" id="all_footwear">
         <h1>Sneakers</h1>
@@ -78,15 +47,15 @@
         <h2><a href="create_sneaker.php">Create sneaker</a></h2>
         <?php endif ?>
         <div id="search_form">
-            <form action="index.php?searched=true" method="post">
+            <form class="form-inline" action="index.php?searched=true" method="post">
                <fieldset>
                   <div class="search">
                   <h2>Search for sneakers!</h2>
                      <p>
-                        <input name="search_input" type="text" id="search_input" style="width:330px;" placeholder="Search by name of sneaker, value, or size of sneaker..." value=""/>
+                        <input class="form-control mr-sm-2" name="search_input" type="search" id="search_input" style="width:400px;" placeholder="Search by name of sneaker, value, or size of sneaker..." value=""/>
                      </p>
                      <p>
-                        <input type="submit" name="command" value="Search" />
+                        <input class="btn btn-primary" type="submit" name="command" value="Search" />
                      </p>
                   </div>
                </fieldset>
@@ -112,38 +81,22 @@
             <h2><a href="index.php?reset=true">View all sneakers</a></h2>
             <?php foreach($searchResult as $sneaker):?>
                 <h2><?=$sneaker['sneaker_name']?> 
-                 <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
+                 <small><a class="badge badge-primary" href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
                 <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
-                || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
+                || <small><a class="badge badge-info" href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
                 <?php endif ?>
             </h2>
             <?php endforeach ?>
         <?php elseif(!$_POST || $_GET['reset'] == true): ?>
         <?php foreach($sneakers as $sneaker): ?>
             <h2><?=$sneaker['sneaker_name']?> 
-                 <small><a href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
+                 <small><a class="badge badge-primary" href="view_sneaker.php?id=<?=$sneaker['sneaker_id']?>">View Sneaker</a></small>
                 <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1):?>
-                || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>">edit</a></small>
+                || <small><a href="edit_sneaker.php?id=<?=$sneaker['sneaker_id']?>" class="badge badge-info">edit</a></small>
                 <?php endif ?>
             </h2>
         <?php endforeach ?>
         <?php endif ?>
     </div>
-    <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1): ?>
-        <div id="all_categories">
-        <h1>Categories</h1>
-        <h2><a href="create_category.php">Create category</a></h2>
-        <?php foreach($categories as $category): ?>
-            <h3>Category name: <?=$category['category_name']?> <small><a href="edit_category.php?id=<?=$category['category_id']?>">edit</a></small></h3>
-        <?php endforeach ?>
-    </div>
-    <div id="all_brands">
-        <h1>Manufacturers</h1>
-        <h2><a href="create_brand.php">Create brand</a></h2>
-        <?php foreach($brands as $brand): ?>
-            <h3>Brand name: <?=$brand['sneaker_brand']?> <small><a href="edit_brand.php?id=<?=$brand['sneaker_brand_id']?>">edit</a></small></h3>
-        <?php endforeach ?>
-    </div>
-    <?php endif ?>
    </body>
 </html>
