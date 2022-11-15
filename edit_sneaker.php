a2befda1d8c8968c688dab25b1395e3a6279aff9
Cole Cianflone <colecianflone123@gmail.com>
2022-11-15 13:34:22 -0600

    Update edit_sneaker.php
    added use of template header.

--- edit_sneaker.php
+++ edit_sneaker.php
@@ -5,7 +5,8 @@
     Purpose: Script to edit a sneaker.
 */
 require("connect.php");
-session_start();
+require("templates/header.php");
+//session_start();
 
 // gets id of post
 $sneaker_id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
@@ -54,12 +55,7 @@ $userEditing = $statement->fetchAll();
    <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
       <title>Sneaker - Edit</title>
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
    </head>
-   <h1><a href="index.php">Home</a></h1>
    <?php if(isset($_GET['redirect']) && $_GET['redirect'] == 'view_sneaker'):?>
       <h2><a href="view_sneaker.php?id=<?=$_GET['id']?>"><-- Return</a></h2>
    <?php endif ?>
