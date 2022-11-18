adce6f4914e298b5bcf02ab88a6b49511168a162
Cole Cianflone <colecianflone123@gmail.com>
2022-11-17 20:07:35 -0600

    Update edit_brand.php
    styling overhaul, conformed to html validity.

--- edit_brand.php
+++ edit_brand.php
@@ -1,50 +1,43 @@
 <?php
-/*
-    Cole Cianflone
-    Nov 7th, 2022
-    Purpose: Script to edit a brand.
-*/
-
-require("connect.php");
-require("templates/header.php");
-
-// gets id of post
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
-
-// prepare query
-$query = "SELECT * FROM sneaker_manufacturer WHERE sneaker_brand_id = :id";
-$statement = $db->prepare($query);
-
-// bind values to insert statement
-$statement->bindValue(":id", $id, PDO::PARAM_INT);
-$statement->execute();
-$brands = $statement->fetchAll();
-
-?>
-<!DOCTYPE html>
-<html>
-   <head>
-   <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Brand - Edit</title>
-   </head>
-   <body>
-         <div id="brand_to_edit">
-            <form action="process_brand_post.php" method="post">
-               <fieldset>
-                  <div class="sneaker_edit">
-                     <p>
-                        <label for="sneaker_brand">Brand Name</label>
-                        <input name="sneaker_brand" id="sneaker_brand" value="<?=$brands[0]['sneaker_brand']?>"/>
-                     </p>
-                     <p>
-                        <input type="hidden" name="sneaker_brand_id" value="<?=$_GET['id']?>" />
-                        <input type="submit" name="command" value="Update" />
-                        <input type="submit" name="command" value="Delete" onclick="return confirm('Are you sure you wish to delete this brand? It will delete any sneakers associated with it.')" />
-                     </p>
-                  </div>
-               </fieldset>
-            </form>
+   /*
+       Cole Cianflone
+       Nov 7th, 2022
+       Purpose: Script to edit a brand.
+   */
+   
+   require("connect.php");
+   require("templates/header.php");
+   
+   // gets id of post
+   $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+   
+   // prepare query
+   $query = "SELECT * FROM sneaker_manufacturer WHERE sneaker_brand_id = :id";
+   $statement = $db->prepare($query);
+   
+   // bind values to insert statement
+   $statement->bindValue(":id", $id, PDO::PARAM_INT);
+   $statement->execute();
+   $brands = $statement->fetchAll();
+   
+   ?>
+<div class="container" id="brand_to_edit" style="padding-top: 40px;">
+   <form action="process_brand_post.php" method="post">
+      <fieldset>
+         <div class="form-group">
+            <p>
+               <label for="sneaker_brand">Brand Name</label>
+               <input class="form-control" name="sneaker_brand" style="max-width: 300px;" id="sneaker_brand" value="<?=$brands[0]['sneaker_brand']?>"/>
+            </p>
          </div>
-   </body>
+         <div class="form-group">
+            <p>
+               <input type="hidden" name="sneaker_brand_id" value="<?=$_GET['id']?>" />
+               <input class="btn btn-success" type="submit" name="command" value="Update" />
+               <input class="btn btn-danger" type="submit" name="command" value="Delete" onclick="return confirm('Are you sure you wish to delete this brand? It will delete any sneakers associated with it.')" />
+            </p>
+         </div>
+      </fieldset>
+   </form>
+</div>
 </html>
