42699d89bf545f86a8bf9bc271bd31e8d482dada
Cole Cianflone <colecianflone123@gmail.com>
2022-11-17 20:07:53 -0600

    Update create_brand.php
    styling overhaul, conformed to html validity.

--- create_brand.php
+++ create_brand.php
@@ -1,36 +1,29 @@
 <?php
-/*
-    Cole Cianflone
-    Nov 7th, 2022
-    Purpose: Script to create a category.
-*/
-
-require("connect.php");
-require("templates/header.php");
-
-?>
-<!DOCTYPE html>
-<html>
-   <head>
-   <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Brand - Create</title>
-   </head>
-   <body>
-         <div id="brand_to_create">
-            <form action="process_brand_post.php" method="post">
-               <fieldset>
-                  <div class="brand">
-                     <p>
-                        <label for="sneaker_brand">Brand Name</label>
-                        <input name="sneaker_brand" id="sneaker_brand" placeholder="Enter a brand name..."value=""/>
-                     </p>
-                     <p>
-                        <input type="submit" name="command" value="Create" />
-                     </p>
-                  </div>
-               </fieldset>
-            </form>
+   /*
+       Cole Cianflone
+       Nov 7th, 2022
+       Purpose: Script to create a category.
+   */
+   
+   require("connect.php");
+   require("templates/header.php");
+   
+   ?>
+<div class="container" id="brand_to_create" style="padding-top: 40px;">
+   <form action="process_brand_post.php" method="post">
+      <fieldset>
+         <div class="form-group">
+            <p>
+               <label for="sneaker_brand">Brand Name</label>
+               <input class="form-control" style="max-width: 300px;" name="sneaker_brand" id="sneaker_brand" placeholder="Enter a brand name..." value=""/>
+            </p>
          </div>
-   </body>
+         <div class="form-group">
+            <p>
+               <input class="btn btn-primary" type="submit" name="command" value="Create" />
+            </p>
+         </div>
+      </fieldset>
+   </form>
+</div>
 </html>
