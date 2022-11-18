7fa8db79dc097b9dffe4597e2064a3c478769650
Cole Cianflone <colecianflone123@gmail.com>
2022-11-17 20:00:54 -0600

    Update admin_brands.php
    Overhauled styling, conformed to html validity.

--- admin_brands.php
+++ admin_brands.php
@@ -1,31 +1,31 @@
 <?php 
-    require("connect.php");
-    require("templates/header.php");
-
-    /* SELECT ALL DATA FROM sneaker_manufacturer table */
-    $query = "SELECT * FROM sneaker_manufacturer";
-    $statement = $db->prepare($query); // Returns a PDOStatement object.
-    $statement->execute(); // The query is now executed.
-    $brands = $statement->fetchAll();
-?>
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Category - Administration</title>
-   </head>
-   <body>
-   <div class="container" id="all_categories">
-    <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1): ?>
-        <div id="all_brands">
-            <h1>Brands</h1>
-            <h2><a href="create_brand.php">Create brand</a></h2>
-            <?php foreach($brands as $brand): ?>
-                <h3><?=$brand['sneaker_brand']?> <small><a class="badge badge-info" href="edit_brand.php?id=<?=$brand['sneaker_brand_id']?>">edit</a></small></h3>
-            <?php endforeach ?>
-        </div>
-    <?php endif ?>
-    </div>
-   </body>
+   require("connect.php");
+   require("templates/header.php");
+   
+   /* SELECT ALL DATA FROM sneaker_manufacturer table */
+   $query = "SELECT * FROM sneaker_manufacturer";
+   $statement = $db->prepare($query); // Returns a PDOStatement object.
+   $statement->execute(); // The query is now executed.
+   $brands = $statement->fetchAll();
+   $counter = 0;
+   ?>
+<div class="container">
+   <?php if(isset($_SESSION['logged_in_user']) && $_SESSION['admin_is_on'] === 1): ?>
+   <div id="all_brands">
+      <h1>Brands</h1>
+      <h2><a href="create_brand.php">Create brand</a></h2>
+      <div class="card-columns">
+         <?php foreach($brands as $brand):?>
+         <div class="card">
+            <div class="card-body">
+               <h5 class="card-title"><?=$brand['sneaker_brand']?></h5>
+               <a href="edit_brand.php?id=<?=$brand['sneaker_brand_id']?>" class="btn btn-primary">Edit Or Delete</a>
+            </div>
+         </div>
+         <!-- <h3><?=$brand['sneaker_brand']?> <small><a class="badge badge-info" href="edit_brand.php?id=<?=$brand['sneaker_brand_id']?>">edit</a></small></h3> -->
+         <?php endforeach ?>
+      </div>
+   </div>
+   <?php endif ?>
+</div>
 </html>
