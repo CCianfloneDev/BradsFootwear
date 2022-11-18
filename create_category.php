316a63542e6893db0eaac95e090575ab27db5a6d
Cole Cianflone <colecianflone123@gmail.com>
2022-11-17 20:17:11 -0600

    Update create_category.php
    styling overhaul, conformed to html validity.

--- create_category.php
+++ create_category.php
@@ -1,36 +1,29 @@
 <?php
-/*
-    Cole Cianflone
-    Nov 7th, 2022
-    Purpose: Script to create a category.
-*/
-
-require("connect.php");
-require("templates/header.php");
-
-?>
-<!DOCTYPE html>
-<html>
-   <head>
-   <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>Category - Create</title>
-   </head>
-   <body>
-         <div id="category_to_create">
-            <form action="process_category_post.php" method="post">
-               <fieldset>
-                  <div class="category">
-                     <p>
-                        <label for="category_name">Category Name</label>
-                        <input name="category_name" id="category_name" placeholder="Enter category name..." value=""/>
-                     </p>
-                     <p>
-                        <input type="submit" name="command" value="Create" />
-                     </p>
-                  </div>
-               </fieldset>
-            </form>
+   /*
+       Cole Cianflone
+       Nov 7th, 2022
+       Purpose: Script to create a category.
+   */
+   
+   require("connect.php");
+   require("templates/header.php");
+   
+   ?>
+<div class="container" id="category_to_create" style="padding-top: 40px;">
+   <form action="process_category_post.php" method="post">
+      <fieldset>
+         <div class="form-group">
+            <p>
+               <label for="category_name">Category Name</label>
+               <input class="form-control" name="category_name" style="max-width: 300px;" id="category_name" placeholder="Enter category name..." value=""/>
+            </p>
          </div>
-   </body>
+         <div class="form-group">
+            <p>
+               <input class="btn btn-primary" type="submit" name="command" value="Create" />
+            </p>
+         </div>
+      </fieldset>
+   </form>
+</div>
 </html>
