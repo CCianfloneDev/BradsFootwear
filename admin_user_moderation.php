0e42ac10e50ade67dd7412ce30d40d36165c0dc4
Cole Cianflone <colecianflone123@gmail.com>
2022-11-17 21:12:14 -0600

    Update admin_user_moderation.php
    style change

--- admin_user_moderation.php
+++ admin_user_moderation.php
@@ -19,14 +19,14 @@
             <?php if($user['admin_access'] == 1):?>
             <?php if($user['user_id'] != $_SESSION['logged_in_user_id']):?>
             <p class="card-text"> Has admin access </p>
-            <a class="btn btn-warning" onclick="return confirm('Are you sure you wish to revoke this users access?')" href="change_access.php?admin=revoke&user_id=<?=$user['user_id']?>">Revoke admin access</a>
+            <a class="btn btn-primary" onclick="return confirm('Are you sure you wish to revoke this users access?')" href="change_access.php?admin=revoke&user_id=<?=$user['user_id']?>">Revoke admin access</a>
             <?php endif ?>
             <?php else: ?>
             <p class="card-text"> Does not have admin access </p>
-            <a class="btn btn-success" onclick="return confirm('Are you sure you wish to grant this user admin access?')" href="change_access.php?admin=grant&user_id=<?=$user['user_id']?>">Grant admin access</a>
+            <a class="btn btn-primary" onclick="return confirm('Are you sure you wish to grant this user admin access?')" href="change_access.php?admin=grant&user_id=<?=$user['user_id']?>">Grant admin access</a>
             <?php endif ?>
             <?php if($user['user_id'] != $_SESSION['logged_in_user_id']):?> 
-            <a class="btn btn-warning" onclick="return confirm('Are you sure you wish to delete this user?')" href="delete_user.php?own_account=false&user_id=<?=$user['user_id']?>">Delete This User</a>
+            <a class="btn btn-danger" onclick="return confirm('Are you sure you wish to delete this user?')" href="delete_user.php?own_account=false&user_id=<?=$user['user_id']?>">Delete This User</a>
             <?php else: ?>
             <a class="btn btn-danger" onclick="return confirm('ARE YOU SURE YOU WISH TO TERMINATE YOUR ACCOUNT?')" href="delete_user.php?own_account=true&user_id=<?=$_SESSION['logged_in_user_id']?>">Terminate your account</a>
             <?php endif ?>
